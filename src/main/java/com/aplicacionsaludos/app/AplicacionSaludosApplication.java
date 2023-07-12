7185cc77cb8e8d11239098294d849c129fa18f9a
Kairos Jesus Adrian Diaz Orozco <nem.jadrian@coppel.com>
2023-07-12 11:24:47 -0700

    Inicializando proyecto

---
+++ src/main/java/com/aplicacionsaludos/app/AplicacionSaludosApplication.java
@@ -0,0 +1,13 @@
+package com.aplicacionsaludos.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionSaludosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionSaludosApplication.class, args);
+	}
+
+}
